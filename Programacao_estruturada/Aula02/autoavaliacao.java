90c916aad885c4c70e9d29b6cce2d5a0c3f5179e
josineidess <josineide1402@gmail.com>
2021-04-22 00:43:52 -0300

    adicionando autoatividade

---
+++ Programacao_estruturada/Aula02/autoavaliacao.java
@@ -0,0 +1,46 @@
+public class Autoavalicao {
+  //Atividade1---------
+  public static int variavelGlobal = 20; //variavel global
+  private static int variavelLocal1 = 30, variavelLocal2 = 40, variavelLocal3 = 50, variavelLocal4 = 60; //variaveis locais
+  public static String g = "g", a = "a", t = "t", o = "o"; //variavel strings globais
+
+  public static void main(String[] args){
+    System.out.println("-----Atividade1-----");
+    System.out.println(variavelGlobal);
+    System.out.println(variavelLocal1);
+    System.out.println(variavelLocal2);
+    System.out.println(variavelLocal3);
+    System.out.println(variavelLocal4);
+    System.out.print(g + a + t + o + "\n");
+    atividade2();
+  }
+  
+  //Atividade2
+  public static void atividade2() {
+    /*
+    int NUMERO, X, SOMA;
+    float MEDIA, K, L;
+
+    SOMA = NUMERO + 2;
+    System.out.println("SOMA: " + SOMA);
+    MEDIA = SOMA;
+    System.out.println("MEDIA: " + MEDIA);
+    NUMERO = K + L;
+    System.out.println("NUMERO: " + NUMERO);
+    X = X + 1;
+    System.out.println("X: " + X);
+    L = SOMA - K;
+    System.out.println("L: " + L);
+    SOMA + 2 = NUMERO + 10;
+    System.out.println("SOMA2: " + SOMA);
+    S = SOMA;
+    System.out.println("S: " + S);
+    X = SOMA - NUMERO;
+    System.out.println("X2: " + X);
+    */
+    System.out.println("-----Atividade2-----");
+    System.out.println("As operações não irão funcionar pois os tipos de alguns são incompatíveis e as variaveis não foram inicializadas.");
+  }
+
+}
+
